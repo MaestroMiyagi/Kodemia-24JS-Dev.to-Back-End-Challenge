9a0ebc968fb85f73a2deac10bb9d3471e5c67d81
JesusMendoza815 <jesuscamposmendoza815@gmail.com>
2023-04-26 18:25:17 -0600

    hotfix: conditional dom

--- src/modules/isExpiredToken.js
+++ src/modules/isExpiredToken.js
@@ -2,9 +2,8 @@
 const isExpiredToken = (logged, logout) => {
   let token = localStorage.getItem("token");
   let tokenInfo;
-  token ? tokenInfo = split('.')[1] : null;
-  tokenInfo ? tokenInfo = JSON.parse(atob(token)) : tokenInfo = false; 
-  
+  token ? tokenInfo = token.split('.')[1] : null;
+  tokenInfo ? tokenInfo = JSON.parse(atob(tokenInfo)) : tokenInfo = false; 
   if (tokenInfo && tokenInfo.exp > moment().unix()) {
     logged.forEach(item => item.classList.add('d-none'))
   } else {
